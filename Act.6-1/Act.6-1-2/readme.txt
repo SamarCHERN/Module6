4fb20f0427615732c09aa82d731d2ae689dd5881
Samar Cherni <scherni@dom.tti>
2022-07-21 09:32:23 +0100

    update

--- Act.6-1/Act.6-1-2/readme.txt
+++ Act.6-1/Act.6-1-2/readme.txt
@@ -1,2 +1,22 @@
-# Installer Guzzle
-composer require guzzlehttp/guzzle:^7.0
+Leçon 6.1 : Client REST
+
+# On clone le dépot 
+https://github.com/SamarCHERN/Module6.git
+
+# On installe les dépendances:
+composer install
+
+# On lance le serveur
+php bin/console server:run
+
+Pour exécuter le code voici les différentes routes pour réaliser des actions HTTP sur "https://jsonplaceholder.typicode.com/" :
+
+@Route ("/"): GET/posts
+@Route ("/blog/{id}"): GET    /posts/id
+@Route ("/post/{id}"): 	GET    /posts?userId=1
+@Route ("/blog/{id}/comments"):GET  /posts/id/comments
+@Route ("/comments/{postid}"): GET  /posts/comments?postId=id
+@Route ("/post"): POST	/posts
+@Route ("/put/{id}"): PUT	/posts/id
+@Route ("/patch/{id}"):PATCH	/posts/id
+@Route ("/delete/{id}"):DELETE	/posts/id
--- Act.6-1/Act.6-1-2/src/Controller/BlogController.php
+++ Act.6-1/Act.6-1-2/src/Controller/BlogController.php
@@ -2,72 +2,145 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use GuzzleHttp\Client;
-use Psr\Http\Message\ResponseInterface;
 
 class BlogController extends AbstractController
 {
-        /**
-     * @Route("/posts", name="home")
+    /**
+     * @Route("/", name="app_blog")
      */
-    public function home(): Response
+    public function GetGuzzleRequest()
     {
-        $client = new Client (['base_uri' => 'https://jsonplaceholder.typicode.com/']);
-        $response = $client->request('GET', '/posts');
-        $body=$response->getBody();
-        $post=json_decode($body);
-        return $this->render('blog/home.html.twig',[
-            'articles'=>$post
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get('https://jsonplaceholder.typicode.com/posts');// Url of your choosing
+        $response = $request->getBody();
+        $articles=json_decode($response);
+        return $this->render('blog/index.html.twig', [
+            'articles'=> $articles
         ]);
     }
 
     /**
-     * @Route("/put/{id}", name="put")
+     * @Route("/blog/{id}", name="id")
      */
-    public function Put(int $id)
+    public function GetIdtGuzzleRequest(int $id)
     {
-        $client = new Client([
-            'base_uri' => 'https://jsonplaceholder.typicode.com',
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get('https://jsonplaceholder.typicode.com/posts/'.$id);// Url of your choosing
+        $response = $request->getBody();
+        $articles=json_decode($response);
+        return $this->render('blog/id.html.twig', [
+            'articles'=> $articles
         ]);
-          
-        $response = $client->request('PUT', '/posts/:id', [
-            'json' => [
-                'title' => 'Samar',
-                'body' => 'cursus symfony'
-            ]
-        ]);
-          
-        $body = $response->getBody();
-        $articles = json_decode($body);
+    }
 
-        return $this->render('blog/show.html.twig',[
-            'articles'=>$articles,
+    /**
+     * @Route("/post/{id}", name="GetUserId")
+     */
+    public function GetUserIdtGuzzleRequest(int $id)
+    {
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get('https://jsonplaceholder.typicode.com/posts?userId='.$id);// Url of your choosing
+        $response = $request->getBody();
+        $articles=json_decode($response);
+        return $this->render('blog/userid.html.twig', [
+            'articles'=> $articles
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}/comments", name="comments")
+     */
+    public function GetCommentstGuzzleRequest(int $id)
+    {
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get('https://jsonplaceholder.typicode.com/posts'."/".$id."/"."comments");// Url of your choosing
+        $response = $request->getBody();
+        $articles=json_decode($response);
+        return $this->render('blog/comments.html.twig', [
+            'articles'=> $articles
+        ]);
+    }
+
+    /**
+     * @Route("/comments/{postid}", name="commentsbypost")
+     */
+    public function GetCommentsBypostGuzzleRequest(int $postid)
+    {
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get('https://jsonplaceholder.typicode.com/comments?postId='.$postid);// Url of your choosing
+        $response = $request->getBody();
+        $articles=json_decode($response);
+        return $this->render('blog/comments.html.twig', [
+            'articles'=> $articles
+        ]);
+    }
+
+    /**
+     * @Route("/post", name="post")
+     */
+    public function PostGuzzleRequest()
+    {
+        $client = new \GuzzleHttp\Client(["base_uri" => "https://jsonplaceholder.typicode.com"]);
+        $options = [
+            'form_params' => [
+                "title" => "new title"
+               ]
+           ]; 
+        $response = $client->post("/posts", $options);
+        return $this->render('blog/post.html.twig', [
+            'articles'=> $response->getBody()
         ]);
     }
 
     
     /**
-     * @Route("/create", name="create)
+     * @Route("/put/{id}", name="put")
      */
-    public function Post()
+    public function PutGuzzleRequest($id)
     {
-        $client = new Client (['base_uri' => 'https://jsonplaceholder.typicode.com']);
-        $response = $client->request('POST', 'http://httpbin.org/post', [
+        $client = new \GuzzleHttp\Client(["base_uri" => "https://jsonplaceholder.typicode.com"]);
+        $options = [
             'form_params' => [
-                'title' => 'abc',
-                'body' => '123',
-            ]
-        ]);
-        $response->getStatusCode();
-        $body=$response->getBody();
-        $post=json_decode($body);  
-        return $this->render('blog/show.html.twig',[
-            'articles'=>$post,
+                "title" => "put title"
+               ]
+           ]; 
+        $response = $client->put("/posts"."/".$id, $options);
+        return $this->render('blog/put.html.twig', [
+            'articles'=> $response->getBody()
         ]);
     }
 
+    /**
+     * @Route("/patch/{id}", name="patch")
+     */
+    public function PatchGuzzleRequest($id)
+    {
+        $client = new \GuzzleHttp\Client(["base_uri" => "https://jsonplaceholder.typicode.com"]);
+        $options = [
+            'form_params' => [
+                "body"=>"new body"
+               ]
+           ]; 
+        $response = $client->patch("/posts"."/".$id, $options);
+        return $this->render('blog/patch.html.twig', [
+            'articles'=> $response->getBody()
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function DeleteGuzzleRequest($id)
+    {
+        $client = new \GuzzleHttp\Client(["base_uri" => "https://jsonplaceholder.typicode.com"]);
+  
+        $response = $client->delete("/posts"."/".$id);
+        return $this->render('blog/delete.html.twig', [
+            'articles'=> $response->getBody()
+        ]);
+    }
 }
